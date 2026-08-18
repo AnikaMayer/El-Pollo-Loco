c41748c06a4f72b51e812f2373fee64408dcfc69
Anika Mayer <mayer.anika@protonmail.com>
2026-08-18 15:56:44 +0200

    add: world-class

---
+++ models/world.class.js
@@ -0,0 +1,41 @@
+class World {
+    character = new Character();
+    enemies = [new Chicken(), new Chicken(), new Chicken()];
+    clouds = [new Cloud()];
+    backgroundObjects = [
+        new BackgroundObject(ImageHub.BACKGROUND.thirdLayer[0], 0),
+    ];
+    canvas;
+    ctx;
+
+    constructor(_canvas) {
+        this.ctx = _canvas.getContext("2d");
+        this.canvas = _canvas;
+        this.draw();
+    }
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.backgroundObjects);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+
+        // draw() wird immer wieder aufgerufen
+        const self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach((_object) => {
+            this.addToMap(_object);
+        });
+    }
+
+    addToMap(mO) {
+        this.ctx.drawImage(mO.img, mO.x, mO.y, mO.width, mO.height);
+    }
+}
