252035584ede7139c3a40a80291cd25500b72415
Anika Mayer <mayer.anika@protonmail.com>
2026-08-18 15:57:57 +0200

    add: imageHub

---
+++ models/img-hub.class.js
@@ -0,0 +1,64 @@
+class ImageHub {
+    static PEPE = {
+        walk: [
+            "img/2_character_pepe/2_walk/W-21.png",
+            "img/2_character_pepe/2_walk/W-22.png",
+            "img/2_character_pepe/2_walk/W-23.png",
+            "img/2_character_pepe/2_walk/W-24.png",
+            "img/2_character_pepe/2_walk/W-25.png",
+            "img/2_character_pepe/2_walk/W-26.png",
+        ],
+        idle: [
+            "img/2_character_pepe/1_idle/idle/I-1.png",
+            "img/2_character_pepe/1_idle/idle/I-2.png",
+            "img/2_character_pepe/1_idle/idle/I-3.png",
+            "img/2_character_pepe/1_idle/idle/I-4.png",
+            "img/2_character_pepe/1_idle/idle/I-5.png",
+            "img/2_character_pepe/1_idle/idle/I-6.png",
+            "img/2_character_pepe/1_idle/idle/I-7.png",
+            "img/2_character_pepe/1_idle/idle/I-8.png",
+            "img/2_character_pepe/1_idle/idle/I-9.png",
+            "img/2_character_pepe/1_idle/idle/I-10.png",
+        ],
+        longIdle: [
+            "img/2_character_pepe/1_idle/long_idle/I-11.png",
+            "img/2_character_pepe/1_idle/long_idle/I-12.png",
+            "img/2_character_pepe/1_idle/long_idle/I-13.png",
+            "img/2_character_pepe/1_idle/long_idle/I-14.png",
+            "img/2_character_pepe/1_idle/long_idle/I-15.png",
+            "img/2_character_pepe/1_idle/long_idle/I-16.png",
+            "img/2_character_pepe/1_idle/long_idle/I-17.png",
+            "img/2_character_pepe/1_idle/long_idle/I-18.png",
+            "img/2_character_pepe/1_idle/long_idle/I-19.png",
+            "img/2_character_pepe/1_idle/long_idle/I-20.png",
+        ],
+    };
+
+    static CHICKEN = {
+        walk: [
+            "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+            "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+            "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+        ],
+    };
+
+    static BACKGROUND = {
+        clouds: [
+            "img/5_background/layers/4_clouds/1.png",
+            "img/5_background/layers/4_clouds/2.png",
+        ],
+        firstLayer: [
+            "img/5_background/layers/1_first_layer/1.png",
+            "img/5_background/layers/1_first_layer/2.png",
+        ],
+        secondLayer: [
+            "img/5_background/layers/2_second_layer/1.png",
+            "img/5_background/layers/2_second_layer/2.png",
+        ],
+        thirdLayer: [
+            "img/5_background/layers/3_third_layer/1.png",
+            "img/5_background/layers/3_third_layer/2.png",
+        ],
+        air: ["img/5_background/layers/air.png"],
+    };
+}
