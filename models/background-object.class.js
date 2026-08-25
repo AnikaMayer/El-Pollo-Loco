04de505a9101a83825ed596a5790226884b9295b
Anika Mayer <mayer.anika@protonmail.com>
2026-08-25 13:49:09 +0200

    refactor: x-position background-objects

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,12 +1,22 @@
 import { MovableObject } from "./movable-object.class.js";
 
 export class BackgroundObject extends MovableObject {
+    x;
+    y = 0;
     width = 720;
     height = 480;
 
+    static xPos = -719;
+    static turn = 0;
+
     constructor(imagePath, x) {
+        if (BackgroundObject.turn === 4) {
+            BackgroundObject.xPos += 719;
+            BackgroundObject.turn = 0;
+        }
+
         super().loadImage(imagePath);
-        this.x = x;
-        this.y = 480 - this.height; //480 = Höhe canvas, das minus Höhe Objekt für passende Position auf y-Achse
+        this.x = BackgroundObject.xPos;
+        BackgroundObject.turn++;
     }
 }
