abb8dce51558a19f038a603694aeb953fd6ca6fd
Anika Mayer <mayer.anika@protonmail.com>
2026-08-26 13:38:21 +0200

    add: offset-frame throwable-object

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -5,6 +5,13 @@ import { MovableObject } from "./movable-object.class.js";
 export class ThrowableObject extends MovableObject {
     imgPath = ImageHub.BOTTLE;
     keepFalling = true;
+    showFrame = true;
+    offset = {
+        top: 10,
+        right: 15,
+        bottom: 10,
+        left: 15,
+    };
 
     constructor(x, y) {
         super().loadImage(this.imgPath.rotation[0]);
@@ -19,6 +26,7 @@ export class ThrowableObject extends MovableObject {
         this.speedY = 30;
         IntervalHub.startInterval(this.applyGravity, 1000 / 25);
         IntervalHub.startInterval(this.flyingBottle, 1000 / 40);
+        this.getRealFrame();
     }
 
     flyingBottle = () => {
